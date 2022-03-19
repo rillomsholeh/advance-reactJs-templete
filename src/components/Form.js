941a0aa4e23c81ee05e9f38aa9bca2a20bed1c9e
rillomsholeh <rmsholeh76@gmail.com>
2022-03-19 15:39:28 +0700

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 // import React Bootstrap components here
-import { Form, Button } from "react-bootstrap";
+import { Form, Button  } from "react-bootstrap";
 
 // import css modules
 import cssModules from "./Form.module.css";
